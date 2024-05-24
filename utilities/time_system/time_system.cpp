cce64d5c9d6c30b3bc07852fd8ac7d970edfb21f
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-05-25 01:17:34 +0800

    Fix: bugs take place in some condition

--- utilities/time_system/time_system.cpp
+++ utilities/time_system/time_system.cpp
@@ -1,32 +1,38 @@
 #include "time_system.hpp"
+#include <iomanip>
 
 Time& Time::AddTime(int add_month, int add_day, int add_hour, int add_minute) {
   minute += add_minute; add_hour += minute / 60;                 minute %= 60;
   hour += add_hour;     add_day += hour / 24;                    hour %= 24;
-  day += add_day;       add_month += day / kDaysOfMonths[month]; day %= kDaysOfMonths[month];
+  day += add_day;
+  if (day > kDaysOfMonths[month]) {
+    add_month += day / kDaysOfMonths[month]; day %= kDaysOfMonths[month];
+  }
   month += add_month;
   return *this;
 }
 
-Time& Time::AddTime_tmp(int add_month, int add_day, int add_hour, int add_minute) {
-  Time tmp = *this;
+Time Time::AddTime_tmp(int add_month, int add_day, int add_hour, int add_minute) {
+  Time tmp(*this);
   return tmp.AddTime(add_month, add_day, add_hour, add_minute);
 }
 
 Time& Time::MinusTime(int minus_month, int minus_day, int minus_hour, int minus_minute) {
-  month -= minus_month;
-  day -= minus_day;
-  if (day < 0) { day += kDaysOfMonths[--month]; }
-  hour -= minus_hour;
-  if (hour < 0) { hour += 24 * (--day); }
   minute -= minus_minute;
-  if (minute < 0) { minute += 60 * (--hour); }
+  if (minute < 0) { minute += 60; --hour; }
+  hour -= minus_hour;
+  if (hour < 0 && day > 0) { hour += 24; --day; }
+  day -= minus_day;
+  if (day < 1 && month > minus_month) { day += kDaysOfMonths[--month]; }
+  month -= minus_month;
   return *this;
 }
 
 bool Time::InOneDay(const Time &rhs) { return month == rhs.month && day == rhs.day; }
 
 bool Time::LessInDate(const Time &rhs) {return month <= rhs.month && day <= rhs.day; }
+
+bool Time::empty() const { return month == 0 && day == 0 && hour == 0 && minute == 0; }
 
 bool Time::operator<(const Time &other) const {
   return (month == other.month ? (day == other.day ? (hour == other.hour ? minute < other.minute : hour < other.hour) :
@@ -41,12 +47,20 @@ bool Time::operator<=(const Time &other) const {
   return *this < other || *this == other;
 }
 
-Time &Time::operator+(const Time &other) const {
+Time Time::operator+(const Time &other) const {
   Time tmp(*this);
   return tmp.AddTime(other.month, other.day, other.hour, other.minute);
 }
 
-Time &Time::operator-(const Time &other) const {
+Time Time::operator-(const Time &other) const {
   Time tmp(*this);
   return tmp.MinusTime(other.month, other.day, other.hour, other.minute);
+}
+
+std::ostream &operator<<(std::ostream &output, Time &time) {
+  output << std::setw(2) << std::setfill('0') << time.month << "-"
+         << std::setw(2) << std::setfill('0') << time.day << " "
+         << std::setw(2) << std::setfill('0') << time.hour << ":"
+         << std::setw(2) << std::setfill('0') << time.minute;
+  return output;
 }
--- utilities/time_system/time_system.hpp
+++ utilities/time_system/time_system.hpp
@@ -1,11 +1,24 @@
 #ifndef TIMESYSTEM_HPP
 #define TIMESYSTEM_HPP
+#include <iostream>
 
-const int kDaysOfMonths[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int kDaysOfMonths[13] = {0x7fffffff, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 struct Time {
+  friend std::ostream& operator << (std::ostream&, Time&);
+  
   Time() {
     month = day = hour = minute = 0;
+  }
+
+  Time(const Time &other) {
+    if (this == &other) {
+      return;
+    }
+    this->month = other.month;
+    this->day = other.day;
+    this->hour = other.hour;
+    this->minute = other.minute;
   }
 
   Time(int month_, int day_, int hour_, int minute_) {
@@ -16,7 +29,7 @@ struct Time {
 
   Time& AddTime(int add_month, int add_day, int add_hour, int add_minute);
 
-  Time& AddTime_tmp(int add_month, int add_day, int add_hour, int add_minute);
+  Time AddTime_tmp(int add_month, int add_day, int add_hour, int add_minute);
 
   Time& MinusTime(int minus_month, int minus_day, int minus_hour, int minus_minute);
 
@@ -24,15 +37,17 @@ struct Time {
 
   bool LessInDate(const Time &rhs);
 
+  bool empty() const;
+
   bool operator<(const Time &other) const;
 
   bool operator==(const Time &other) const;
 
   bool operator<=(const Time &other) const;
 
-  Time& operator+(const Time &other) const;
+  Time operator+(const Time &other) const;
 
-  Time &operator-(const Time &other) const;
+  Time operator-(const Time &other) const;
 };
 
 #endif //TIMESYSTEM_HPP
