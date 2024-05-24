1b06c47ec8114ffbafcc1b5cc21a00eee2a00301
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-05-25 01:16:18 +0800

    Add: support to certain parameter

--- utilities/parameter_type/parameter_type.hpp
+++ utilities/parameter_type/parameter_type.hpp
@@ -21,13 +21,23 @@ enum ParameterType {
   HELP,
   UNDEFINED_PARAMETER,
   NOT_A_PARAMETER,
+  NUMBER,
+  START_STATION,
+  END_STATION,
   OPTION
 };
 
 enum EnumType {
   USER,
   TRAIN,
+  SELLING,
   OTHER
+};
+
+enum Status {
+  SUCCESS = 0,
+  PENDING = 1,
+  REFUNDED = 2
 };
 
 #endif //ParameterType_HPP
