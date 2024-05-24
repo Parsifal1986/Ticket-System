8de04468d365a094d8991f373e5e6c5caaf1d9d6
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-05-25 01:14:51 +0800

    Change: the member variable Add: an output function

--- utilities/log_system/log_system.cpp
+++ utilities/log_system/log_system.cpp
@@ -1,13 +1,15 @@
 #include "log_system.hpp"
+#include <iostream>
+#include <ostream>
 
 LogSystem::LogSystem() : log_database("log_data") {
 
 }
 
-void LogSystem::AddLog(UserName username, TrainID& train_id, std::string& start_pos, std::string& end_pos, Time& start_time,
-       Time& end_time, int price, int number, short status, short start_p, short end_p) {
+void LogSystem::AddLog(UserName username, TrainID& train_id, std::string& start_pos, std::string& end_pos, Time start_time,
+       Time end_time, Time start_date, int price, int number, Status status, short start_p, short end_p) {
   log_database.Insert(username, Log(train_id, start_pos, end_pos, start_time,
-                                    end_time, price, number, status, start_p, end_p));
+                                    end_time, start_date, price, number, status, start_p, end_p, time_stamp));
 }
 
 sjtu::vector<Log>* LogSystem::QueryLog(UserName username) {
@@ -17,14 +19,32 @@ sjtu::vector<Log>* LogSystem::QueryLog(UserName username) {
     ret = log_database.Find(username);
   } catch (Exception *error) {
     delete error;
-    throw new NoSuchLog();
+    ret = new sjtu::vector<Log>;
   }
 
   return ret;
 }
 
+void LogSystem::Output(std::ostream& output, sjtu::vector<Log>* list) {
+  output << list->size() << std::endl;
+
+  for (int i = 0; i < list->size(); i++) {
+    output << "[" << mapping[list->at(i).status] << "] "
+           << list->at(i).train_id.train_id << " " << list->at(i).start_pos
+           << " " << list->at(i).start_time << " -> " << list->at(i).end_pos
+           << " " << list->at(i).end_time << " " << list->at(i).price << " "
+           << list->at(i).number << std::endl;
+  }
+
+  return;
+}
+
 Log LogSystem::QueryLog(UserName username, int number) {
   sjtu::vector<Log> *ret = QueryLog(username);
+
+  if (ret->size() < number) {
+    throw new NoSuchLog;
+  }
 
   Log log = ret->at(number - 1);
   delete ret;
@@ -32,7 +52,7 @@ Log LogSystem::QueryLog(UserName username, int number) {
   return log;
 }
 
-void LogSystem::Update(UserName username, int number, int new_status) {
+void LogSystem::Update(UserName username, int number, Status new_status) {
   Log log = QueryLog(username, number);
 
   log_database.Delete(username, log);
@@ -42,15 +62,15 @@ void LogSystem::Update(UserName username, int number, int new_status) {
   log_database.Insert(username, log);
 }
 
-bool LogSystem::Update(UserName username, int time_stamp_, int new_status, bool flag) {
-  sjtu::vector<Log> *ret = QueryLog(username);
+bool LogSystem::Update(UserName username, int time_stamp_, Status new_status, bool flag) {
+  sjtu::vector<Log> *ret = log_database.Find(username);
 
   for (int i = 0; i < ret->size(); i++) {
-    if (ret->at(i).time_stamp == time_stamp_) {
+    if (ret->at(i).time_stamp_ == time_stamp_) {
       log_database.Delete(username, ret->at(i));
       ret->at(i).status = new_status;
       log_database.Insert(username, ret->at(i));
-      return true;;
+      return true;
     }
   }
   return false;
--- utilities/log_system/log_system.hpp
+++ utilities/log_system/log_system.hpp
@@ -7,9 +7,11 @@
 #include "../time_system/time_system.hpp"
 #include "../user_manager/user_manager.hpp"
 #include "../train_manager/train_manager.hpp"
+#include "../parameter_type/parameter_type.hpp"
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <ostream>
 #include <string>
 
 extern int time_stamp;
@@ -18,25 +20,27 @@ struct Log {
   Log() {}
 
   Log(const TrainID &train_id_, const std::string &start_pos_,
-      const std::string &end_pos_, const Time &start_time_,
-      const Time &end_time_, const int &price_, const int &number_,
-      const short &status_, const short start_p_, const short end_p_)
-      : train_id(train_id_), start_time(start_time_), end_time(end_time_),
-        price(price_), number(number_), status(status_), start_p(start_p_), end_p(end_p_) {
+      const std::string &end_pos_, const Time &start_time_, const Time &end_time_,
+      const Time &start_date_, const int &price_, const int &number_,
+      const Status &status_, const short start_p_, const short end_p_, const int time_stamp)
+      : train_id(train_id_), start_time(start_time_), end_time(end_time_),start_date(start_date_),
+        price(price_), number(number_), status(status_), start_p(start_p_), end_p(end_p_), time_stamp_(time_stamp) {
     std::strcpy(start_pos, start_pos_.c_str());
+    std::strcpy(end_pos, end_pos_.c_str());
   }
 
   ~Log() {}
 
-  short status, start_p, end_p;
+  Status status;
+  short start_p, end_p;
   TrainID train_id;
   char start_pos[31], end_pos[31];
-  Time start_time, end_time;
-  int price, number, time_stamp;
+  Time start_time, end_time, start_date;
+  int price, number, time_stamp_;
 
-  bool operator<(const Log &other) { return time_stamp < other.time_stamp; }
+  bool operator<(const Log &other) { return time_stamp_ > other.time_stamp_; }
 
-  bool operator==(const Log &other) { return time_stamp == other.time_stamp; }
+  bool operator==(const Log &other) { return time_stamp_ == other.time_stamp_; }
 };
 
 class LogSystem {
@@ -45,18 +49,21 @@ public:
 
   ~LogSystem() = default;
 
-  void AddLog(UserName username, TrainID& train_id, std::string& start_pos, std::string& end_pos, Time& start_time,
-       Time& end_time, int price, int number, short status, short start_p, short end_p);
+  void AddLog(UserName username, TrainID& train_id, std::string& start_pos, std::string& end_pos, Time start_time,
+       Time end_time, Time start_date, int price, int number, Status status, short start_p, short end_p);
 
   sjtu::vector<Log>* QueryLog(UserName username);
 
   Log QueryLog(UserName username, int number);
   
-  void Update(UserName username, int number, int status);
+  void Update(UserName username, int number, Status status);
 
-  bool Update(UserName username, int time_stamp_, int status, bool flag);
+  bool Update(UserName username, int time_stamp_, Status status, bool flag);
+
+  void Output(std::ostream &output, sjtu::vector<Log>* list);
 private:
   BpTree<UserName, Log> log_database;
+  std::string mapping[3] = {"success", "pending", "refunded"};
 };
 
 #endif // LOG_SYSTEM_HPP
