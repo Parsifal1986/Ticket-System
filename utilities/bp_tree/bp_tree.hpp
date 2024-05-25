a95845825d21699c09e6077420cab1d8ab42a864
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-05-25 15:24:06 +0800

    Change: reduce the cache size

--- utilities/bp_tree/bp_tree.hpp
+++ utilities/bp_tree/bp_tree.hpp
@@ -208,7 +208,7 @@ private:
                          // help in Ticket System)
   size_t root_position_;
 
-  LruCache<Node, 10007, size_t(3e5 + 1)> cache;
+  LruCache<Node, 10007, size_t(2e5 + 1)> cache;
 
   std::fstream file_;
 
